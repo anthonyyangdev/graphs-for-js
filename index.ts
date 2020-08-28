9c2a7409c54343e89c4422e2fa140125a625e749
Anthony Yang <ay339@cornell.edu>
2020-08-28 16:54:45 -0400

    Add graph builder

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
+import { WeightedUndirectedGraph } from './src/WeightedUndirectedGraph'
+import { UndirectedGraph } from './src/UndirectedGraph'
+import { DirectedGraph } from './src/DirectedGraph'
+import { WeightedDirectedGraph } from './src/WeightedDirectedGraph'
 
+const builder = <V, E>(fn?: (v: V) => string) => {
+  return {
+    directed: {
+      weighted: () => new WeightedDirectedGraph<V, E>(fn),
+      unweighted: () => new DirectedGraph<V>(fn)
+    },
+    undirected: {
+      weighted: () => new WeightedUndirectedGraph<V, E>(fn),
+      unweighted: () => new UndirectedGraph<V>(fn)
+    }
+  }
+}
+
+export const GraphBuilder = <V, E> () => {
+  return {
+    withKeyFunction: (fn: (v: V) => string) => builder<V, E>(fn),
+    withoutKeyFunction: () => builder<V, E>()
+  }
+}
