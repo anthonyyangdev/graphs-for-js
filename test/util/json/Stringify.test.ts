94392bcc55a1979b35216c9f09981e5b50061e33
Anthony Yang <ay339@cornell.edu>
2020-08-29 17:35:37 -0400

    Add stringify test

---
+++ test/util/json/Stringify.test.ts
@@ -0,0 +1,59 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import { DirectedGraph } from '../../../src/DirectedGraph'
+import { stringify } from '../../../src/util/json'
+import { UndirectedGraph } from '../../../src/UndirectedGraph'
+import { WeightedDirectedGraph } from '../../../src/WeightedDirectedGraph'
+import { WeightedUndirectedGraph } from '../../../src/WeightedUndirectedGraph'
+import { GraphInterface } from '../../../src/types/GraphInterface'
+
+describe('Test stringify', function () {
+  it('should stringify empty graph', function () {
+    let graph: GraphInterface<number> = new DirectedGraph<number>()
+    let json = stringify(graph).replace(/ /g, '')
+    expect(json.includes('"nodes":[]')).is.true
+    expect(json.includes('"edges":[]')).is.true
+    expect(json.includes('"undirected":false')).is.true
+    expect(json.includes('"weighted":false')).is.true
+
+    graph = new UndirectedGraph<number>()
+    json = stringify(graph).replace(/ /g, '')
+    expect(json.includes('"nodes":[]')).is.true
+    expect(json.includes('"edges":[]')).is.true
+    expect(json.includes('"undirected":true')).is.true
+    expect(json.includes('"weighted":false')).is.true
+
+    graph = new WeightedDirectedGraph<number, number>()
+    json = stringify(graph).replace(/ /g, '')
+    expect(json.includes('"nodes":[]')).is.true
+    expect(json.includes('"edges":[]')).is.true
+    expect(json.includes('"undirected":false')).is.true
+    expect(json.includes('"weighted":true')).is.true
+
+    graph = new WeightedUndirectedGraph<number, number>()
+    json = stringify(graph).replace(/ /g, '')
+    expect(json.includes('"nodes":[]')).is.true
+    expect(json.includes('"edges":[]')).is.true
+    expect(json.includes('"undirected":true')).is.true
+    expect(json.includes('"weighted":true')).is.true
+  })
+
+  it('should stringify into readable graph json', function () {
+    const graph = new DirectedGraph<number>()
+    graph.insert(0, 1, 2)
+    graph.connect(1, 2)
+    const json = stringify(graph).replace(/\s/g, '')
+    expect(json.includes('"nodes":[0,1,2]')).is.true
+    expect(json.includes('"edges":[{"source":1,"target":2}]')).is.true
+  })
+
+  it('should emit each edge once for undirected graph', function () {
+    const graph = new UndirectedGraph<number>()
+    graph.insert(0, 1)
+    graph.connect(0, 1)
+    const json = stringify(graph).replace(/\s/g, '')
+    expect(json.includes('"nodes":[0,1]'))
+    expect((json.includes('"edges":[{"source":0,"target":1}]') ? 1 : 0) ^
+      (json.includes('"edges":[{"source":1,"target":0}]') ? 1 : 0)).equals(1)
+  })
+})
