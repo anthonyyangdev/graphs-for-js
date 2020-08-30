e6e70c19da3c585a9129f4e819a0ad2a714b6024
Anthony Yang <ay339@cornell.edu>
2020-08-29 22:08:06 -0400

    Add test in weighted undirected graph for connect/disconnect

--- test/WeightedUndirectedGraph.test.ts
+++ test/WeightedUndirectedGraph.test.ts
@@ -3,12 +3,30 @@ import { expect } from 'chai'
 import { GraphType } from '../src/types/GraphType'
 import { WeightedUndirectedGraph } from '../src/WeightedUndirectedGraph'
 
-describe('Weighted directed graph', function () {
+describe('Weighted undirected graph', function () {
   it('should instantiate', function () {
     const graph = new WeightedUndirectedGraph<number, number>()
     expect(graph.edges().length).equals(0)
     expect(graph.nodes().length).equals(0)
     expect(graph.getGraphType()).equals(GraphType.WeightedUndirected)
+  })
+
+  it('should return false for failed unconnected edges', function () {
+    const graph = new WeightedUndirectedGraph<number, number>()
+    graph.insert(0, 1)
+    expect(graph.connect(-2, 2, 5)).is.false
+    expect(graph.connect(0, 2, 0.75)).is.false
+    expect(graph.connect(0, 1, 0.75)).is.true
+    expect(graph.connect(1, 0, 0.75)).is.false
+
+    expect(graph.connect(1, 0, 100)).is.true
+
+    expect(graph.disconnect(0, 10, -1)).is.false
+    expect(graph.disconnect(1, 0, -1)).is.false
+    expect(graph.disconnect(1, 0, 100)).is.true
+
+    expect(graph.hasEdge(0, 1)).is.false
+    expect(graph.hasEdge(1, 0)).is.false
   })
 
   it('should have two way edges', function () {
