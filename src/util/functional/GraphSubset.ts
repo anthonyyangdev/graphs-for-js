0d8ace7e7254b84a25539b756ac634b817725944
Anthony Yang <ay339@cornell.edu>
2020-08-30 14:01:01 -0400

    Add subset nodes

--- src/util/functional/GraphSubset.ts
+++ src/util/functional/GraphSubset.ts
@@ -2,37 +2,41 @@ import { IGeneralNodeGraph, IReadonlyGeneralNodeGraph } from '../../types/GraphI
 import { GraphType } from '../../types/GraphType'
 import { DirectedGraph, WeightedDirectedGraph } from '../../mutable/DirectedGraphs'
 import { UndirectedGraph, WeightedUndirectedGraph } from '../../mutable/UndirectedGraphs'
+import { Set } from 'typescript-collections'
 
-export const mapNodes = <V, E, N> (
+export const subsetNode = <V, E> (
   g: IReadonlyGeneralNodeGraph<V, E>,
-  callback: (v: V) => N,
-  newKeyFunction?: (n: N) => string
+  nodes: V[]
 ) => {
-  const edges = g.edges()
-  const nodes = g.nodes()
+  const setOfNodes = new Set<V>(g.toKeyFn)
+  nodes.forEach(n => setOfNodes.add(n))
 
-  let clone: IGeneralNodeGraph<N, E>
+  let clone: IGeneralNodeGraph<V, E>
   switch (g.getGraphType()) {
     case GraphType.WeightedDirected:
     case GraphType.ReadonlyWeightedDirected:
-      clone = new WeightedDirectedGraph<N, E>(newKeyFunction)
+      clone = new WeightedDirectedGraph<V, E>(g.toKeyFn)
       break
     case GraphType.NonWeightedDirected:
     case GraphType.ReadonlyNonWeightedDirected:
-      clone = new DirectedGraph<N, E>(newKeyFunction)
+      clone = new DirectedGraph<V, E>(g.toKeyFn)
       break
     case GraphType.WeightedUndirected:
     case GraphType.ReadonlyWeightedUndirected:
-      clone = new WeightedUndirectedGraph<N, E>(newKeyFunction)
+      clone = new WeightedUndirectedGraph<V, E>(g.toKeyFn)
       break
     case GraphType.NonWeightedUndirected:
     case GraphType.ReadonlyNonWeightedUndirected:
-      clone = new UndirectedGraph<N, E>(newKeyFunction)
+      clone = new UndirectedGraph<V, E>(g.toKeyFn)
       break
   }
-  clone.insert(...nodes.map(n => callback(n)))
-  edges.forEach(({ source, value, target }) => {
-    clone.connect(callback(source), callback(target), value)
+  g.nodes().forEach(n => {
+    if (setOfNodes.contains(n)) clone.insert(n)
+  })
+  g.edges().forEach(({ source, value, target }) => {
+    if (clone.contains(source, target)) {
+      clone.connect(source, target, value)
+    }
   })
   return clone
 }
